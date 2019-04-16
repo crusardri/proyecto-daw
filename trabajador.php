2e11422f7b973008c1afdb9c9db1970b0adc801d
crusardri <crusardgameamos@gmail.com>
2019-04-16 21:06:30 +0200

    Terminado Plantilla Trabajador

---
+++ trabajador.php
@@ -0,0 +1,44 @@
+<?php 
+require("Funciones/vistaController.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Trabajador</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <?php include("./includes/navbar.inc") ?>
+    <div class="worker-container">
+        <section class="buttons-container">
+            <a class="haptic-button medium">
+                <img src="media/img/new-order.png">
+                <div class="label">Registrar Órden</div>
+            </a>
+            <a class="haptic-button medium">
+                <img src="media/img/see-order.png">
+                <div class="label">Ver Órdenes</div>
+            </a>
+            <a class="haptic-button medium">
+                <img src="media/img/add-user.png">
+                <div class="label">Registrar Usuario</div>
+            </a>
+            <a class="haptic-button medium">
+                <img src="media/img/see-users.png">
+                <div class="label">Ver Usuarios</div>
+            </a>
+        </section>
+        <section class="my-orders orders-container">
+           <?php showOrdersShowcase("Órdenes Asignadas a mí."); ?>
+        </section>
+        <section class="all-orders orders-container">
+           <?php showOrdersShowcase("Todas las ordenes."); ?>
+        </section>
+        <section class="updates orders-container">
+           <?php showOrdersShowcase("Actualizaciones."); ?>
+        </section>
+    </div>
+</body>
+</html>
