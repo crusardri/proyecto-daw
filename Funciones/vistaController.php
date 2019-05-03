d7076de18b9aadaf19487a46434e96101074c0b8
crusardri <crusardgameamos@gmail.com>
2019-05-03 14:49:55 +0200

    creado labels para los formularios, creado un nuevo estado "Cancelado"

--- Funciones/vistaController.php
+++ Funciones/vistaController.php
@@ -38,7 +38,40 @@ function showOrdersShowcase($titulo){
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
             </a>
-
+            <a href="" class="order-item canceled">
+                <div class="estate canceled"></div>
+                <div class="order-info">
+                    <div class="item-id a-center">
+                        <span class="label">ID: </span>
+                        <span class="content">000001</span>
+                    </div>
+                    <div class="item-id">
+                        <span class="label">Estado: </span>
+                        <span class="content label-box canceled">Cancelado</span>
+                    </div>
+                    <div class="item-id">
+                        <span class="label">Asignado a: </span>
+                        <span class="content">Iván Maldonado</span>
+                    </div>
+                    <div class="item-id">
+                        <span class="label">Cliente: </span>
+                        <span class="content">Halfonso Fernandez</span>
+                    </div>
+                </div>
+                <div class="order-fetch">
+                    <div class="item-id">
+                        <span class="label">Entrada: </span>
+                        <span class="content">14/04/2019</span>
+                    </div>
+                    <div class="item-id">
+                        <span class="label">Previsto: </span>
+                        <span class="content">17/04/2019</span>
+                    </div>
+                </div>
+                <div class="order-desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </a>
         </div>
         
     </div>
@@ -202,6 +235,20 @@ function showOrdersTable(){
             <tr class="order pending">
                 <td class="id a-center"><span class="responsive-label">ID</span> <a href="">000001</a></td>
                 <td><span class="responsive-label">Estado</span> <a href="" class="label-box pending"> Pendiente</a></td>
+                <td class="a-center"><span class="responsive-label">Nº Prendas</span> 2</td>
+                <td class="a-center"><span class="responsive-label">Precio</span> 10€</td>
+                <td><span class="responsive-label">Asignado a</span> <a href="">Iván Maldonado</a></td>
+                <td><span class="responsive-label">Cliente</span> <a href="">Halfonso Fernández</a></td>
+                <td class="date"><span class="responsive-label">Entrada</span> 17/04/2019 13:00</td>
+                <td class="date"><span class="responsive-label">Previsto</span> 20/04/2019 13:00</td>
+                <td class="date hidden"><span class="responsive-label">Terminado</span> -</td>
+                <td class="date hidden"><span class="responsive-label">Entregado</span> -</td>
+                <td class="date"><span class="responsive-label">Actualizado</span> 17/04/2019 13:00</td>
+                <td class="desc"><span class="desc-cont">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></td>
+            </tr>
+            <tr class="order canceled">
+                <td class="id a-center"><span class="responsive-label">ID</span> <a href="">000001</a></td>
+                <td><span class="responsive-label">Estado</span> <a href="" class="label-box canceled">Cancelado</a></td>
                 <td class="a-center"><span class="responsive-label">Nº Prendas</span> 2</td>
                 <td class="a-center"><span class="responsive-label">Precio</span> 10€</td>
                 <td><span class="responsive-label">Asignado a</span> <a href="">Iván Maldonado</a></td>
--- newOrder.php
+++ newOrder.php
@@ -7,96 +7,61 @@
 </head>
 <body>
     <?php include("./includes/navbar.inc") ?>
-    <form>
-       <label class="boxed-radio">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Por Defecto</div>
-                <div class="radio-desc">Por defecto.</div>
-            </div>
-        </label>
-        <label class="boxed-radio">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Por Defecto</div>
-                <div class="radio-desc">Por defecto.</div>
-            </div>
-        </label>
-        <label class="boxed-radio active">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Activo</div>
-                <div class="radio-desc">El usuario puede iniciar sesión en la aplicación y utilizar todas sus funciones.</div>
-            </div>
-        </label>
-        <label class="boxed-radio disabled">
-            <input type="radio" name="estate" name="1" checked>
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Desactivado</div>
-                <div class="radio-desc">El usuario no podrá iniciar sesión, todos sus datos se mantendran en la aplicación para consulta.</div>
-            </div>
-        </label>
-        
-        <label class="boxed-radio pending">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Pendiente</div>
-                <div class="radio-desc">La orden ha sido recibida y esta en espera.</div>
-            </div>
-        </label>
-        <label class="boxed-radio working">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">En proceso</div>
-                <div class="radio-desc">La orden esta realizandose.</div>
-            </div>
-        </label>
-        <label class="boxed-radio finished">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Finalizado</div>
-                <div class="radio-desc">La orden ha sido terminada y esta pendiente de recogida.</div>
-            </div>
-        </label>
-        <label class="boxed-radio out">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Entregado</div>
-                <div class="radio-desc">La orden ha sido recogida.</div>
-            </div>
-        </label>
-        <label class="boxed-radio client">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Cliente</div>
-                <div class="radio-desc">Nivel de usuario que solo permite relaccionar sus órdenes.</div>
-            </div>
-        </label>
-        <label class="boxed-radio employee">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Empleado</div>
-                <div class="radio-desc">Nivel de usuario que permite crear y modificar ordenes y usuarios.</div>
-            </div>
-        </label>
-        <label class="boxed-radio admin">
-            <input type="radio" name="estate" name="1">
-            <div class="container">
-                <div class="radio-checkbox">&#x2713;</div>
-                <div class="radio-title">Administrador</div>
-                <div class="radio-desc">Nivel de usuario que da control total sobre la aplicación.</div>
-            </div>
-        </label>
-    </form>
+    <div class="form-container new-order">
+        <h1>Nueva órden</h1>
+        <form>
+            <label class="boxed-input id">
+                <div class="text-label">ID</div>
+                <input type="number" value="000001" disabled>
+            </label>
+            <label class="boxed-input worker">
+                <div class="text-label">Número de prendas</div>
+                <input type="number" value="3" disabled>
+                <div class="button">Buscar</div>
+            </label>
+            
+            
+            <label class="boxed-radio pending">
+                <input type="radio" name="estate" name="1" checked>
+                <div class="container">
+                    <div class="radio-checkbox">&#x2713;</div>
+                    <div class="radio-title">Pendiente</div>
+                    <div class="radio-desc">La orden ha sido recibida y esta en espera.</div>
+                </div>
+            </label>
+            <label class="boxed-radio working">
+                <input type="radio" name="estate" name="1">
+                <div class="container">
+                    <div class="radio-checkbox">&#x2713;</div>
+                    <div class="radio-title">En proceso</div>
+                    <div class="radio-desc">La orden esta realizandose.</div>
+                </div>
+            </label>
+            <label class="boxed-radio finished">
+                <input type="radio" name="estate" name="1">
+                <div class="container">
+                    <div class="radio-checkbox">&#x2713;</div>
+                    <div class="radio-title">Finalizado</div>
+                    <div class="radio-desc">La orden ha sido terminada y esta pendiente de recogida.</div>
+                </div>
+            </label>
+            <label class="boxed-radio out">
+                <input type="radio" name="estate" name="1">
+                <div class="container">
+                    <div class="radio-checkbox">&#x2713;</div>
+                    <div class="radio-title">Entregado</div>
+                    <div class="radio-desc">La orden ha sido recogida.</div>
+                </div>
+            </label>
+            <label class="boxed-radio canceled">
+                <input type="radio" name="estate" name="1">
+                <div class="container">
+                    <div class="radio-checkbox">&#x2713;</div>
+                    <div class="radio-title">Cancelado</div>
+                    <div class="radio-desc">La orden ha sido cancelada.</div>
+                </div>
+            </label>
+        </form>
+    </div>
 </body>
 </html>
