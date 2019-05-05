a4ffc2696900e86ee963364b8e33b3ff03933c7e
crusardri <crusardgameamos@gmail.com>
2019-05-05 22:59:58 +0200

    Primera iteracion del popup modal, creado un layout para order

--- order.php
+++ order.php
@@ -3,22 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Nueva Orden</title>
+    <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/main.js"></script>
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     <?php include("./includes/navbar.inc") ?>
-    <div class="form-container new-order">
+    <div class="form-container order">
         
         <form>
-            <h1>Nueva órden</h1>
-            <label class="boxed-input id">
-                <div class="text-label">ID</div>
-                <input type="number" value="000001" disabled>
-            </label>
+            <div class="order-info">
+                <h1>Nueva órden</h1>
+                <label class="boxed-input id">
+                    <div class="text-label">ID</div>
+                    <input type="number" value="000001" disabled>
+                </label>
+            </div>      
             <!-- User Info -->
-            <br>
-            <div class="form-col-50">
+            <div class="client-info">
                 <h3>Cliente</h3>
                 <div class="field-set">
                     <div class="info-set">
@@ -45,10 +47,10 @@
                         <div class="form-info-title">Telefono</div>
                         <div class="form-info-data">689966332</div>
                     </div>
-                    <div class="button">Cambiar</div>
+                    <div class="button" id="search-client">Cambiar</div>
                 </div>
             </div>
-            <div class="form-col-50">
+            <div class="employee-info">
                 <h3>Empleado</h3>
                 <div class="field-set">
                     <div class="info-set">
@@ -67,52 +69,56 @@
                         <div class="form-info-title">Apellidos</div>
                         <div class="form-info-data">Maldonado</div>
                     </div>
-                    <div class="button">Cambiar</div>
+                    <div class="button" id="search-employee">Cambiar</div>
                 </div>
             </div>
              
             <!-- Estados -->
-            <h2>Estado</h2>
-            <label class="boxed-radio pending">
-                <input type="radio" name="estate" name="1" checked>
-                <div class="container">
-                    <div class="radio-checkbox">&#x2713;</div>
-                    <div class="radio-title">Pendiente</div>
-                    <div class="radio-desc">La orden ha sido recibida y esta en espera.</div>
-                </div>
-            </label>
-            <label class="boxed-radio working">
-                <input type="radio" name="estate" name="1">
-                <div class="container">
-                    <div class="radio-checkbox">&#x2713;</div>
-                    <div class="radio-title">En proceso</div>
-                    <div class="radio-desc">La orden esta realizandose.</div>
-                </div>
-            </label>
-            <label class="boxed-radio finished">
-                <input type="radio" name="estate" name="1">
-                <div class="container">
-                    <div class="radio-checkbox">&#x2713;</div>
-                    <div class="radio-title">Finalizado</div>
-                    <div class="radio-desc">La orden ha sido terminada y esta pendiente de recogida.</div>
-                </div>
-            </label>
-            <label class="boxed-radio out">
-                <input type="radio" name="estate" name="1">
-                <div class="container">
-                    <div class="radio-checkbox">&#x2713;</div>
-                    <div class="radio-title">Entregado</div>
-                    <div class="radio-desc">La orden ha sido recogida.</div>
-                </div>
-            </label>
-            <label class="boxed-radio canceled">
-                <input type="radio" name="estate" name="1">
-                <div class="container">
-                    <div class="radio-checkbox">&#x2713;</div>
-                    <div class="radio-title">Cancelado</div>
-                    <div class="radio-desc">La orden ha sido cancelada.</div>
-                </div>
-            </label>
+            
+            <div class="estate-container">
+               <h2>Estado</h2>
+                <label class="boxed-radio pending">
+                    <input type="radio" name="estate" name="1" checked>
+                    <div class="container">
+                        <div class="radio-checkbox">&#x2713;</div>
+                        <div class="radio-title">Pendiente</div>
+                        <div class="radio-desc">La orden ha sido recibida y esta en espera.</div>
+                    </div>
+                </label>
+                <label class="boxed-radio working">
+                    <input type="radio" name="estate" name="1">
+                    <div class="container">
+                        <div class="radio-checkbox">&#x2713;</div>
+                        <div class="radio-title">En proceso</div>
+                        <div class="radio-desc">La orden esta realizandose.</div>
+                    </div>
+                </label>
+                <label class="boxed-radio finished">
+                    <input type="radio" name="estate" name="1">
+                    <div class="container">
+                        <div class="radio-checkbox">&#x2713;</div>
+                        <div class="radio-title">Finalizado</div>
+                        <div class="radio-desc">La orden ha sido terminada y esta pendiente de recogida.</div>
+                    </div>
+                </label>
+                <label class="boxed-radio out">
+                    <input type="radio" name="estate" name="1">
+                    <div class="container">
+                        <div class="radio-checkbox">&#x2713;</div>
+                        <div class="radio-title">Entregado</div>
+                        <div class="radio-desc">La orden ha sido recogida.</div>
+                    </div>
+                </label>
+                <label class="boxed-radio canceled full-width">
+                    <input type="radio" name="estate" name="1">
+                    <div class="container">
+                        <div class="radio-checkbox">&#x2713;</div>
+                        <div class="radio-title">Cancelado</div>
+                        <div class="radio-desc">La orden ha sido cancelada.</div>
+                    </div>
+                </label>
+            </div>
+            
         </form>
     </div>
 </body>
