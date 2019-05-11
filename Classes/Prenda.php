604c7a0b42d8f3ccbe52796b56ab70ce3dbab1f1
jhoseph garcia <andre_jhosept@hotmail.com>
2019-05-11 20:16:35 +0200

    Añadido creationDate a Prenda

--- Classes/Prenda.php
+++ Classes/Prenda.php
@@ -3,10 +3,12 @@ class Prenda{
     private $id;
     private $name;
     private $arreglos;
-    function __construct($id, $name, $arreglos){
+    private $creationDate;
+    function __construct($id, $name, $arreglos, $creationDate){
         $this->id = $id;
         $this->name = $name;
         $this->arreglos = $arreglos;
+        $this->creationDate = new DateTime($this->creationDate);
     }
     function getId(){
         return $this->id;
@@ -16,5 +18,13 @@ class Prenda{
     }
     function getArreglos(){
         return $this->arreglos;
+    }
+    function getCreationDate(){   
+        return $this->creationDate;
+    }       
+    function getCreationDateString(){    
+        $date = $this->creationDate;
+        $date->format('d-m-Y H:i:s');   
+        return $this->creationDate;
     }
 }
