562532ada68b064bd15c0761a759be4afd2ba9ab
jhoseph garcia <andre_jhosept@hotmail.com>
2019-05-11 20:31:21 +0200

    Añadido creationDate a Arreglo

--- Classes/Arreglo.php
+++ Classes/Arreglo.php
@@ -3,10 +3,12 @@ class Arreglo{
     private $id;
     private $name;
     private $price;
-    function __construct($id, $name, $price){
+    private $creationDate;
+    function __construct($id, $name, $price, $creationDate){
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
+        $this->creationDate = new DateTime($this->creationDate);
     }
     function getId(){
         return $this->id;
@@ -16,5 +18,13 @@ class Arreglo{
     }
     function getPrice(){
         return $this->price;
+    }
+    function getCreationDate(){   
+        return $this->creationDate;
+    }       
+    function getCreationDateString(){    
+        $date = $this->creationDate;
+        $date->format('d-m-Y H:i:s');   
+        return $this->creationDate;
     }
 }
