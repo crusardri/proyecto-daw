0ccee2ea0b6fa5803521e25bcb634667de06541c
crusardri <crusardgameamos@gmail.com>
2019-05-30 01:31:08 +0200

    cambiado placeholder busqueda prendas

--- clothes.php
+++ clothes.php
@@ -30,7 +30,7 @@
             <label class="boxed-input searchbox">
                 <div class="text-label"><span>Buscar</span></div>
                 <div class="input-container">
-                    <input type="text" placeholder="Buscar usuario por ID, Nombre de usuario, Correo, Nombre, Apellidos, o Teléfono " name="search" value="<?=$search?>" autofocus>
+                    <input type="text" placeholder="Buscar prenda por ID o nombre " name="search" value="<?=$search?>" autofocus>
                 </div>
             </label>
             <?=showStateFilter()?>
